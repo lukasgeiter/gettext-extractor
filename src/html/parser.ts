ceacd5540d34a4b56595d750cabb288bf1b9bef1
Lukas Eipert <git@leipert.io>
2018-06-03 13:36:26 +0200

    Update parse5

--- src/html/parser.ts
+++ src/html/parser.ts
@@ -3,24 +3,24 @@ import * as parse5 from 'parse5';
 import { Parser, IAddMessageCallback, IParseOptions } from '../parser';
 import { IMessage } from '../builder';
 
-export type Node = parse5.AST.Default.Node;
-export type TextNode = parse5.AST.Default.TextNode;
-export type Element = parse5.AST.Default.Element;
+export type Node = parse5.DefaultTreeNode;
+export type TextNode = parse5.DefaultTreeTextNode;
+export type Element = parse5.DefaultTreeElement;
 
 export type IHtmlExtractorFunction = (node: Node, fileName: string, addMessage: IAddMessageCallback) => void;
 
 export class HtmlParser extends Parser<IHtmlExtractorFunction, IParseOptions> {
 
     protected parse(source: string, fileName: string, options: IParseOptions = {}): IMessage[] {
-        let document = parse5.parse(source, {locationInfo: true});
+        let document = parse5.parse(source, {sourceCodeLocationInfo: true});
         return this.parseNode(document, fileName, options.lineNumberStart || 1);
     }
 
     protected parseNode(node: any, fileName: string, lineNumberStart: number): IMessage[] {
         let messages: IMessage[] = [];
         let addMessageCallback = Parser.createAddMessageCallback(messages, fileName, () => {
-            if (node.__location && node.__location.line) {
-                return lineNumberStart + node.__location.line - 1;
+            if (node.sourceCodeLocation && node.sourceCodeLocation.startLine) {
+                return lineNumberStart + node.sourceCodeLocation.startLine - 1;
             }
         });
 
