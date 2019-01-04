7afb89d3068d42ccbf85d19c7a2210de8db95303
Lukas Geiter <info@lukasgeiter.com>
2019-01-05 00:09:26 +0100

    Test normalize content function directly

--- tests/html/utils.test.ts
+++ tests/html/utils.test.ts
@@ -1,7 +1,6 @@
 import * as parse5 from 'parse5';
 import { HtmlUtils } from '../../src/html/utils';
 import { Element } from '../../src/html/parser';
-import { IContentOptions } from '../../src/utils/content';
 
 describe('HTML: Utils', () => {
 
@@ -36,405 +35,34 @@ describe('HTML: Utils', () => {
         });
     });
 
-    function registerGetElementContentTests(newLine: string): void {
-        const SINGLE_LINE = `<p>FooBar</p>`;
-        const SINGLE_LINE_WITH_ELEMENT = `<p>Foo<strong>Bar</strong></p>`;
-        const SINGLE_LINE_ON_SEPARATE_LINE = `<p>${newLine}FooBar${newLine}</p>`;
-        const SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_SPACES = `<p>${newLine}    FooBar${newLine}</p>`;
-        const SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_TABS = `<p>${newLine}\tFooBar${newLine}</p>`;
-        const SINGLE_LINE_WITH_MULTIPLE_LEADING_NEWLINES = `<p>${newLine}${newLine}FooBar</p>`;
-        const SINGLE_LINE_WITH_MULTIPLE_TRAILING_NEWLINES = `<p>FooBar${newLine}${newLine}</p>`;
-
-        const MULTIPLE_LINES = `<p>${newLine}Foo${newLine}Bar${newLine}</p>`;
-        const MULTIPLE_LINES_INDENTED_SPACES = `<p>${newLine}    Foo${newLine}    Bar${newLine}</p>`;
-        const MULTIPLE_LINES_INDENTED_TABS = `<p>${newLine}\tFoo${newLine}\tBar${newLine}</p>`;
-
-        const MULTIPLE_CHILD_ELEMENTS = `<div>${newLine}<div>${newLine}<p>Foo</p>${newLine}<p>Bar</p>${newLine}</div>${newLine}</div>`;
-        const MULTIPLE_CHILD_ELEMENTS_INDENTED_SPACES = `<div>${newLine}    <div>${newLine}        <p>Foo</p>${newLine}        <p>Bar</p>${newLine}    </div>${newLine}</div>`;
-        const MULTIPLE_CHILD_ELEMENTS_INDENTED_TABS = `<div>${newLine}\t<div>${newLine}\t\t<p>Foo</p>${newLine}\t\t<p>Bar</p>${newLine}\t</div>${newLine}</div>`;
-
-        let options: IContentOptions;
+    describe('getElementContent', () => {
 
         function getContent(source: string): string {
-            return HtmlUtils.getElementContent(createElement(source), options);
+            return HtmlUtils.getElementContent(createElement(source), {
+                preserveIndentation: true,
+                trimWhiteSpace: true,
+                replaceNewLines: false
+            });
         }
 
-        describe('normal', () => {
-
-            beforeEach(() => {
-                options = {
-                    preserveIndentation: false,
-                    replaceNewLines: false,
-                    trimWhiteSpace: true
-                };
-            });
-
-            test('single line of text', () => {
-                expect(getContent(SINGLE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text with element', () => {
-                expect(getContent(SINGLE_LINE_WITH_ELEMENT)).toBe('Foo<strong>Bar</strong>');
-            });
-
-            test('single line of text on separate line', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text on separate line indented with spaces', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_SPACES)).toBe('FooBar');
-            });
-
-            test('single line of text on separate line indented with tabs', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_TABS)).toBe('FooBar');
-            });
-
-            test('single line of text with multiple leading newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_LEADING_NEWLINES)).toBe('FooBar');
-            });
-
-            test('single line of text with multiple trailing newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_TRAILING_NEWLINES)).toBe('FooBar');
-            });
-
-            test('multiple lines of text', () => {
-                expect(getContent(MULTIPLE_LINES)).toBe('Foo\nBar');
-            });
-
-            test('multiple lines of text indented with spaces', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_SPACES)).toBe('Foo\nBar');
-            });
-
-            test('multiple lines of text indented with tabs', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_TABS)).toBe('Foo\nBar');
-            });
-
-            test('multiple child elements', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS)).toBe('<div>\n<p>Foo</p>\n<p>Bar</p>\n</div>');
-            });
-
-            test('multiple child elements indented with spaces', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_SPACES)).toBe('<div>\n<p>Foo</p>\n<p>Bar</p>\n</div>');
-            });
-
-            test('multiple child elements indented with tabs', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_TABS)).toBe('<div>\n<p>Foo</p>\n<p>Bar</p>\n</div>');
-            });
+        test('single line', () => {
+            expect(getContent('<div>Foo Bar</div>')).toBe('Foo Bar');
         });
 
-        describe('don\'t trim whitespace', () => {
-
-            beforeEach(() => {
-                options = {
-                    preserveIndentation: false,
-                    replaceNewLines: false,
-                    trimWhiteSpace: false
-                };
-            });
-
-            test('single line of text', () => {
-                expect(getContent(SINGLE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text with element', () => {
-                expect(getContent(SINGLE_LINE_WITH_ELEMENT)).toBe('Foo<strong>Bar</strong>');
-            });
-
-            test('single line of text on separate line', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE)).toBe('\nFooBar\n');
-            });
-
-            test('single line of text on separate line indented with spaces', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_SPACES)).toBe('\nFooBar\n');
-            });
-
-            test('single line of text on separate line indented with tabs', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_TABS)).toBe('\nFooBar\n');
-            });
-
-            test('single line of text with multiple leading newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_LEADING_NEWLINES)).toBe('\n\nFooBar');
-            });
-
-            test('single line of text with multiple trailing newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_TRAILING_NEWLINES)).toBe('FooBar\n\n');
-            });
-
-            test('multiple lines of text', () => {
-                expect(getContent(MULTIPLE_LINES)).toBe('\nFoo\nBar\n');
-            });
-
-            test('multiple lines of text indented with spaces', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_SPACES)).toBe('\nFoo\nBar\n');
-            });
-
-            test('multiple lines of text indented with tabs', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_TABS)).toBe('\nFoo\nBar\n');
-            });
-
-            test('multiple child elements', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS)).toBe('\n<div>\n<p>Foo</p>\n<p>Bar</p>\n</div>\n');
-            });
-
-            test('multiple child elements indented with spaces', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_SPACES)).toBe('\n<div>\n<p>Foo</p>\n<p>Bar</p>\n</div>\n');
-            });
-
-            test('multiple child elements indented with tabs', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_TABS)).toBe('\n<div>\n<p>Foo</p>\n<p>Bar</p>\n</div>\n');
-            });
+        test('nested element', () => {
+            expect(getContent('<div>Foo <strong>Bar</strong></div>')).toBe('Foo <strong>Bar</strong>');
         });
 
-        describe('preserve indentation', () => {
-
-            beforeEach(() => {
-                options = {
-                    preserveIndentation: true,
-                    replaceNewLines: false,
-                    trimWhiteSpace: true
-                };
-            });
-
-            test('single line of text', () => {
-                expect(getContent(SINGLE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text with element', () => {
-                expect(getContent(SINGLE_LINE_WITH_ELEMENT)).toBe('Foo<strong>Bar</strong>');
-            });
-
-            test('single line of text on separate line', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text on separate line indented with spaces', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_SPACES)).toBe('    FooBar');
-            });
-
-            test('single line of text on separate line indented with tabs', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_TABS)).toBe('\tFooBar');
-            });
-
-            test('single line of text with multiple leading newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_LEADING_NEWLINES)).toBe('FooBar');
-            });
-
-            test('single line of text with multiple trailing newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_TRAILING_NEWLINES)).toBe('FooBar');
-            });
-
-            test('multiple lines of text', () => {
-                expect(getContent(MULTIPLE_LINES)).toBe('Foo\nBar');
-            });
-
-            test('multiple lines of text indented with spaces', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_SPACES)).toBe('    Foo\n    Bar');
-            });
-
-            test('multiple lines of text indented with tabs', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_TABS)).toBe('\tFoo\n\tBar');
-            });
-
-            test('multiple child elements', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS)).toBe('<div>\n<p>Foo</p>\n<p>Bar</p>\n</div>');
-            });
-
-            test('multiple child elements indented with spaces', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_SPACES)).toBe('    <div>\n        <p>Foo</p>\n        <p>Bar</p>\n    </div>');
-            });
-
-            test('multiple child elements indented with tabs', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_TABS)).toBe('\t<div>\n\t\t<p>Foo</p>\n\t\t<p>Bar</p>\n\t</div>');
-            });
-        });
-
-        describe('preserve indentation and don\'t trim whitespace', () => {
-
-            beforeEach(() => {
-                options = {
-                    preserveIndentation: true,
-                    replaceNewLines: false,
-                    trimWhiteSpace: false
-                };
-            });
-
-            test('single line of text', () => {
-                expect(getContent(SINGLE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text with element', () => {
-                expect(getContent(SINGLE_LINE_WITH_ELEMENT)).toBe('Foo<strong>Bar</strong>');
-            });
-
-            test('single line of text on separate line', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE)).toBe('\nFooBar\n');
-            });
-
-            test('single line of text on separate line indented with spaces', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_SPACES)).toBe('\n    FooBar\n');
-            });
-
-            test('single line of text on separate line indented with tabs', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_TABS)).toBe('\n\tFooBar\n');
-            });
-
-            test('single line of text with multiple leading newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_LEADING_NEWLINES)).toBe('\n\nFooBar');
-            });
-
-            test('single line of text with multiple trailing newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_TRAILING_NEWLINES)).toBe('FooBar\n\n');
-            });
-
-            test('multiple lines of text', () => {
-                expect(getContent(MULTIPLE_LINES)).toBe('\nFoo\nBar\n');
-            });
-
-            test('multiple lines of text indented with spaces', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_SPACES)).toBe('\n    Foo\n    Bar\n');
-            });
-
-            test('multiple lines of text indented with tabs', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_TABS)).toBe('\n\tFoo\n\tBar\n');
-            });
-
-            test('multiple child elements', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS)).toBe('\n<div>\n<p>Foo</p>\n<p>Bar</p>\n</div>\n');
-            });
-
-            test('multiple child elements indented with spaces', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_SPACES)).toBe('\n    <div>\n        <p>Foo</p>\n        <p>Bar</p>\n    </div>\n');
-            });
-
-            test('multiple child elements indented with tabs', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_TABS)).toBe('\n\t<div>\n\t\t<p>Foo</p>\n\t\t<p>Bar</p>\n\t</div>\n');
-            });
-        });
-
-        describe('replace newlines with space', () => {
-
-            beforeEach(() => {
-                options = {
-                    preserveIndentation: false,
-                    replaceNewLines: ' ',
-                    trimWhiteSpace: true
-                };
-            });
-
-            test('single line of text', () => {
-                expect(getContent(SINGLE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text with element', () => {
-                expect(getContent(SINGLE_LINE_WITH_ELEMENT)).toBe('Foo<strong>Bar</strong>');
-            });
-
-            test('single line of text on separate line', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text on separate line indented with spaces', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_SPACES)).toBe('FooBar');
-            });
-
-            test('single line of text on separate line indented with tabs', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_TABS)).toBe('FooBar');
-            });
-
-            test('single line of text with multiple leading newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_LEADING_NEWLINES)).toBe('FooBar');
-            });
-
-            test('single line of text with multiple trailing newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_TRAILING_NEWLINES)).toBe('FooBar');
-            });
-
-            test('multiple lines of text', () => {
-                expect(getContent(MULTIPLE_LINES)).toBe('Foo Bar');
-            });
-
-            test('multiple lines of text indented with spaces', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_SPACES)).toBe('Foo Bar');
-            });
-
-            test('multiple lines of text indented with tabs', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_TABS)).toBe('Foo Bar');
-            });
-
-            test('multiple child elements', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS)).toBe('<div> <p>Foo</p> <p>Bar</p> </div>');
-            });
-
-            test('multiple child elements indented with spaces', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_SPACES)).toBe('<div> <p>Foo</p> <p>Bar</p> </div>');
-            });
-
-            test('multiple child elements indented with tabs', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_TABS)).toBe('<div> <p>Foo</p> <p>Bar</p> </div>');
-            });
-        });
-
-        describe('replace newlines with space and preserve indentation', () => {
-
-            beforeEach(() => {
-                options = {
-                    preserveIndentation: true,
-                    replaceNewLines: ' ',
-                    trimWhiteSpace: true
-                };
-            });
-
-            test('single line of text', () => {
-                expect(getContent(SINGLE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text with element', () => {
-                expect(getContent(SINGLE_LINE_WITH_ELEMENT)).toBe('Foo<strong>Bar</strong>');
-            });
-
-            test('single line of text on separate line', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE)).toBe('FooBar');
-            });
-
-            test('single line of text on separate line indented with spaces', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_SPACES)).toBe('    FooBar');
-            });
-
-            test('single line of text on separate line indented with tabs', () => {
-                expect(getContent(SINGLE_LINE_ON_SEPARATE_LINE_INDENTED_TABS)).toBe('\tFooBar');
-            });
-
-            test('single line of text with multiple leading newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_LEADING_NEWLINES)).toBe('FooBar');
-            });
-
-            test('single line of text with multiple trailing newlines', () => {
-                expect(getContent(SINGLE_LINE_WITH_MULTIPLE_TRAILING_NEWLINES)).toBe('FooBar');
-            });
-
-            test('multiple lines of text', () => {
-                expect(getContent(MULTIPLE_LINES)).toBe('Foo Bar');
-            });
-
-            test('multiple lines of text indented with spaces', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_SPACES)).toBe('    Foo     Bar');
-            });
-
-            test('multiple lines of text indented with tabs', () => {
-                expect(getContent(MULTIPLE_LINES_INDENTED_TABS)).toBe('\tFoo \tBar');
-            });
-
-            test('multiple child elements', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS)).toBe('<div> <p>Foo</p> <p>Bar</p> </div>');
-            });
-
-            test('multiple child elements indented with spaces', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_SPACES)).toBe('    <div>         <p>Foo</p>         <p>Bar</p>     </div>');
-            });
-
-            test('multiple child elements indented with tabs', () => {
-                expect(getContent(MULTIPLE_CHILD_ELEMENTS_INDENTED_TABS)).toBe('\t<div> \t\t<p>Foo</p> \t\t<p>Bar</p> \t</div>');
-            });
+        test('indentation', () => {
+            expect(getContent(
+                '<div>\n' +
+                '   Foo\n' +
+                '   Bar\n' +
+                '</div>'
+            )).toBe(
+                '   Foo\n' +
+                '   Bar'
+            );
         });
 
         describe('un-escaping', () => {
@@ -451,15 +79,5 @@ describe('HTML: Utils', () => {
                 expect(getContent('<div>Foo > Bar</div>')).toBe('Foo > Bar');
             });
         });
-    }
-
-    describe('getElementContent', () => {
-
-        registerGetElementContentTests('\n');
-    });
-
-    describe('getElementContent CRLF', () => {
-
-        registerGetElementContentTests('\r\n');
     });
 });
---
+++ tests/utils/content.test.ts
@@ -0,0 +1,240 @@
+import { IContentOptions, normalizeContent } from '../../src/utils/content';
+
+describe('Content Utils', () => {
+
+    describe('normalizeContent', () => {
+
+        type Scenario = 'default' | 'noTrim' | 'preserveIndentation' | 'noTrimPreserveIndentation' | 'replaceNewlinesCRLF' | 'replaceNewlinesCRLFPreserveIndentation';
+
+        const scenarios: {[scenario in Scenario]: IContentOptions} = {
+            default: {
+                preserveIndentation: false,
+                replaceNewLines: false,
+                trimWhiteSpace: true
+            },
+            noTrim: {
+                preserveIndentation: false,
+                replaceNewLines: false,
+                trimWhiteSpace: false
+            },
+            preserveIndentation: {
+                preserveIndentation: true,
+                replaceNewLines: false,
+                trimWhiteSpace: true
+            },
+            noTrimPreserveIndentation: {
+                preserveIndentation: true,
+                replaceNewLines: false,
+                trimWhiteSpace: false
+            },
+            replaceNewlinesCRLF: {
+                preserveIndentation: false,
+                replaceNewLines: '\r\n',
+                trimWhiteSpace: true
+            },
+            replaceNewlinesCRLFPreserveIndentation: {
+                preserveIndentation: true,
+                replaceNewLines: '\r\n',
+                trimWhiteSpace: true
+            }
+        };
+
+        function registerNormalizeContentTests(newLine: string, whitespace: string): void {
+
+            function testCase(summary: string, source: string, expectedResults: { [scenario in Scenario]: string }): void {
+                const whitespacePlaceholder = / {4}/g;
+                source = source.replace(/\n/g, newLine).replace(whitespacePlaceholder, whitespace);
+
+                describe(summary, () => {
+                    for (let scenario of Object.keys(scenarios)) {
+                        test(scenario, () => {
+                            expect(normalizeContent(source, scenarios[scenario]))
+                                .toBe(expectedResults[scenario].replace(whitespacePlaceholder, whitespace));
+                        });
+                    }
+                });
+            }
+
+            testCase('single line',
+                'Foo Bar',
+                {
+                    default:
+                        'Foo Bar',
+                    noTrim:
+                        'Foo Bar',
+                    preserveIndentation:
+                        'Foo Bar',
+                    noTrimPreserveIndentation:
+                        'Foo Bar',
+                    replaceNewlinesCRLF:
+                        'Foo Bar',
+                    replaceNewlinesCRLFPreserveIndentation:
+                        'Foo Bar'
+                }
+            );
+
+            testCase('leading and trailing newline',
+                '\n' +
+                'Foo Bar\n',
+                {
+                    default:
+                        'Foo Bar',
+                    noTrim:
+                        '\n' +
+                        'Foo Bar\n',
+                    preserveIndentation:
+                        'Foo Bar',
+                    noTrimPreserveIndentation:
+                        '\n' +
+                        'Foo Bar\n',
+                    replaceNewlinesCRLF:
+                        'Foo Bar',
+                    replaceNewlinesCRLFPreserveIndentation:
+                        'Foo Bar'
+                }
+            );
+
+            testCase('leading and trailing newline with indentation',
+                '\n' +
+                '    Foo Bar\n',
+                {
+                    default:
+                        'Foo Bar',
+                    noTrim:
+                        '\n' +
+                        'Foo Bar\n',
+                    preserveIndentation:
+                        '    Foo Bar',
+                    noTrimPreserveIndentation:
+                        '\n' +
+                        '    Foo Bar\n',
+                    replaceNewlinesCRLF:
+                        'Foo Bar',
+                    replaceNewlinesCRLFPreserveIndentation:
+                        '    Foo Bar'
+                }
+            );
+
+            testCase('multiple leading newlines',
+                '\n' +
+                '\n' +
+                'Foo Bar',
+                {
+                    default:
+                        'Foo Bar',
+                    noTrim:
+                        '\n' +
+                        '\n' +
+                        'Foo Bar',
+                    preserveIndentation:
+                        'Foo Bar',
+                    noTrimPreserveIndentation:
+                        '\n' +
+                        '\n' +
+                        'Foo Bar',
+                    replaceNewlinesCRLF:
+                        'Foo Bar',
+                    replaceNewlinesCRLFPreserveIndentation:
+                        'Foo Bar'
+                }
+            );
+
+            testCase('multiple trailing newlines',
+                'Foo Bar\n' +
+                '\n',
+                {
+                    default:
+                        'Foo Bar',
+                    noTrim:
+                        'Foo Bar\n' +
+                        '\n',
+                    preserveIndentation:
+                        'Foo Bar',
+                    noTrimPreserveIndentation:
+                        'Foo Bar\n' +
+                        '\n',
+                    replaceNewlinesCRLF:
+                        'Foo Bar',
+                    replaceNewlinesCRLFPreserveIndentation:
+                        'Foo Bar'
+                }
+            );
+
+            testCase('multiple content lines',
+                '\n' +
+                'Foo\n' +
+                'Bar\n',
+                {
+                    default:
+                        'Foo\n' +
+                        'Bar',
+                    noTrim:
+                        '\n' +
+                        'Foo\n' +
+                        'Bar\n',
+                    preserveIndentation:
+                        'Foo\n' +
+                        'Bar',
+                    noTrimPreserveIndentation:
+                        '\n' +
+                        'Foo\n' +
+                        'Bar\n',
+                    replaceNewlinesCRLF:
+                        'Foo\r\n' +
+                        'Bar',
+                    replaceNewlinesCRLFPreserveIndentation:
+                        'Foo\r\n' +
+                        'Bar'
+                }
+            );
+
+            testCase('multiple content lines with indentation',
+                '\n' +
+                '    Foo\n' +
+                '    Bar\n',
+                {
+                    default:
+                        'Foo\n' +
+                        'Bar',
+                    noTrim:
+                        '\n' +
+                        'Foo\n' +
+                        'Bar\n',
+                    preserveIndentation:
+                        '    Foo\n' +
+                        '    Bar',
+                    noTrimPreserveIndentation:
+                        '\n' +
+                        '    Foo\n' +
+                        '    Bar\n',
+                    replaceNewlinesCRLF:
+                        'Foo\r\n' +
+                        'Bar',
+                    replaceNewlinesCRLFPreserveIndentation:
+                        '    Foo\r\n' +
+                        '    Bar'
+                }
+            );
+        }
+
+        describe('LF & spaces', () => {
+
+            registerNormalizeContentTests('\n', '    ');
+        });
+
+        describe('LF & tabs', () => {
+
+            registerNormalizeContentTests('\n', '\t');
+        });
+
+        describe('CRLF & spaces', () => {
+
+            registerNormalizeContentTests('\r\n', '    ');
+        });
+
+        describe('CRLF & tabs', () => {
+
+            registerNormalizeContentTests('\r\n', '\t');
+        });
+    });
+});
