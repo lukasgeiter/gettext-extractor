83a3fff35fd2c74829a9e2f0e5f9ee11a1f18160
youthlin <youthlin.chen@bytedance.com>
2023-02-13 15:29:20 +0800

    attribute predicate

--- src/html/extractors/factories/embeddedAttributeJs.ts
+++ src/html/extractors/factories/embeddedAttributeJs.ts
@@ -3,12 +3,20 @@ import { JsParser } from '../../../js/parser';
 import { Validate } from '../../../utils/validate';
 import { Element, IHtmlExtractorFunction, Node } from '../../parser';
 
-export type AttributePredicate = null // all
-    | string // attribute name pattern
-    | ((attribute: Attribute) => boolean);
+export type AttributePredicate = (attribute: Attribute) => boolean;
 
-export function embeddedAttributeJsExtractor(filter: AttributePredicate, jsParser: JsParser): IHtmlExtractorFunction {
+export function embeddedAttributeJsExtractor(filter: null | string | AttributePredicate, jsParser: JsParser): IHtmlExtractorFunction {
     Validate.required.argument({ jsParser });
+    let test: AttributePredicate = attr => true;
+    if (typeof filter === 'string') {
+        const namePattern = filter
+        test = (attr: Attribute) => {
+            if (attr.name.match(namePattern)) { return true }
+            return false
+        }
+    } else if (typeof filter === 'function') {
+        test = filter
+    }
 
     return (node: Node, fileName: string, _, lineNumberStart) => {
         if (typeof (node as Element).tagName !== 'string') {
@@ -17,19 +25,7 @@ export function embeddedAttributeJsExtractor(filter: AttributePredicate, jsParse
 
         const element = node as Element;
 
-        element.attrs.forEach((attr) => {
-            if (filter) {
-                if (typeof filter === 'string') {
-                    const namePattern = filter
-                    filter = attr => {
-                        if (attr.name.match(namePattern)) { return true }
-                        return false
-                    }
-                }
-                if ((typeof filter === 'function') && !filter(attr)) {
-                    return
-                }
-            }
+        element.attrs.filter(test).forEach((attr) => {
             const startLine = element.sourceCodeLocation?.attrs[attr.name]?.startLine;
             if (startLine) {
                 lineNumberStart = lineNumberStart + startLine - 1;
