f0b38f98aaa9a9d8df2b99f4591a807bb027a0ae
youthlin <youthlin.chen@bytedance.com>
2023-03-06 09:58:53 +0800

    fix test naming

--- tests/html/extractors/factories/embeddedAttributeJs.test.ts
+++ tests/html/extractors/factories/embeddedAttributeJs.test.ts
@@ -1,57 +1,60 @@
-import { embeddedAttributeJsExtractor } from '../../../../src/html/extractors/factories/embeddedAttributeJs';
-import { HtmlParser } from '../../../../src/html/parser';
-import { JsParser } from '../../../../src/js/parser';
+import { embeddedAttributeJsExtractor } from "../../../../src/html/extractors/factories/embeddedAttributeJs";
+import { HtmlParser } from "../../../../src/html/parser";
+import { JsParser } from "../../../../src/js/parser";
 
-describe('HTML: Attribute Value as Embedded JS Extractor', () => {
-
-    describe('calling js parser', () => {
-
-        let htmlParser: HtmlParser,
-            htmlAttrParser: HtmlParser,
-            jsParserMock: JsParser;
+describe("HTML: Attribute Value as Embedded JS Extractor", () => {
+    describe("calling js parser", () => {
+        let jsParserMock: JsParser;
 
         beforeEach(() => {
             jsParserMock = <any>{
-                parseString: jest.fn()
+                parseString: jest.fn(),
             };
+        });
 
-            htmlParser = new HtmlParser(undefined!, [
-                embeddedAttributeJsExtractor(/:title/, jsParserMock,)
+        test("use regex filter / with line number start", () => {
+            const htmlParser = new HtmlParser(undefined!, [
+                embeddedAttributeJsExtractor(/:title/, jsParserMock),
             ]);
-            htmlAttrParser = new HtmlParser(undefined!, [
-                embeddedAttributeJsExtractor(e => { return e.name.startsWith(':') }, jsParserMock,)
+            htmlParser.parseString(
+                `<span :title="__('msg id')" :class="abc">content</span>`,
+                "foo.html",
+                { lineNumberStart: 10 }
+            );
+            expect(jsParserMock.parseString).toHaveBeenCalledWith(
+                `__('msg id')`,
+                "foo.html",
+                {
+                    lineNumberStart: 10,
+                }
+            );
+        });
+
+        test("use filter function", () => {
+            const htmlParser = new HtmlParser(undefined!, [
+                embeddedAttributeJsExtractor((e) => {
+                    return e.name.startsWith(":");
+                }, jsParserMock),
             ]);
+            htmlParser.parseString(
+                `<span :title="__('title')" class="title">Hello</span>`,
+                "foo.html"
+            );
+            expect(jsParserMock.parseString).toHaveBeenCalledWith(
+                `__('title')`,
+                "foo.html",
+                { lineNumberStart: 1 }
+            );
         });
-
-        test('single line', () => {
-            htmlParser.parseString(`<span :title="__('msg id')" title="__('another')">content</span>`, 'foo.html');
-            expect(jsParserMock.parseString).toHaveBeenCalledWith(`__('msg id')`, 'foo.html', {
-                lineNumberStart: 1
-            });
-        });
-
-        test('with lineNumberStart option', () => {
-            htmlParser.parseString(`<span :title="__('msg id')" :class="abc">content</span>`, 'foo.html', { lineNumberStart: 10 });
-            expect(jsParserMock.parseString).toHaveBeenCalledWith(`__('msg id')`, 'foo.html', {
-                lineNumberStart: 10
-            });
-        });
-
-        test('filter attr', () => {
-            htmlAttrParser.parseString(`<span :title="__('title')" class="title">Hello</span>`, 'foo.html');
-            expect(jsParserMock.parseString).toHaveBeenCalledWith(`__('title')`, 'foo.html', { lineNumberStart: 1 });
-        })
-
     });
 
-    describe('argument validation', () => {
-
-        test('filter: (none)', () => {
+    describe("argument validation", () => {
+        test("filter: (none)", () => {
             expect(() => {
                 (<any>embeddedAttributeJsExtractor)();
             }).toThrowError(`Missing argument 'filter'`);
         });
-        test('jsParser: (none)', () => {
+        test("jsParser: (none)", () => {
             expect(() => {
                 (<any>embeddedAttributeJsExtractor)(/:title/);
             }).toThrowError(`Missing argument 'jsParser'`);
