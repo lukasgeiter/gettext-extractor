50175b34aacb8a32c02824ed73df2180be186be5
Jefferson Carvalho <jefferson.carvalho@evolux.net.br>
2023-05-17 09:12:41 -0300

    update htmlTemplate test to use a built in html extractor

--- tests/js/extractors/factories/htmlTemplate.test.ts
+++ tests/js/extractors/factories/htmlTemplate.test.ts
@@ -1,7 +1,5 @@
-import { DefaultTreeTextNode } from 'parse5';
-
 import { htmlTemplateExtractor } from '../../../../src/js/extractors/factories/htmlTemplate'
-import { JsExtractors } from '../../../../src/js/extractors';
+import { HtmlExtractors } from '../../../../src/html/extractors';
 import { JsParser } from '../../../../src/js/parser';
 import { HtmlParser } from '../../../../src/html/parser';
 import { CatalogBuilder, IMessage } from '../../../../src/builder';
@@ -10,8 +8,7 @@ describe('JS: HTML template extractor', () => {
   describe('calling html parser ', () => {
     let builder: CatalogBuilder;
     let messages: IMessage[]
-    let outerJsParser: JsParser;
-    let innerJsParser: JsParser;
+    let jsParser: JsParser;
     let htmlParser: HtmlParser;
 
     beforeEach(() => {
@@ -23,28 +20,17 @@ describe('JS: HTML template extractor', () => {
         })
       };
 
-      innerJsParser = new JsParser(builder, [
-        JsExtractors.callExpression('service.getText', { arguments: { text: 0, context: 1 } })
-      ]);
-
       htmlParser = new HtmlParser(builder, [
-        (node, fileName, _, lineNumberStart) => {
-          if (node.nodeName === '#text') {
-            const textNode = node as DefaultTreeTextNode;
-            const startLineFromNode = textNode.sourceCodeLocation?.startLine;
-            const lineNumber = startLineFromNode ? lineNumberStart + startLineFromNode - 1 : lineNumberStart;
-            innerJsParser.parseString(textNode.value, fileName, { lineNumberStart: lineNumber });
-          }
-        }
+        HtmlExtractors.elementContent('translate')
       ]);
 
-      outerJsParser = new JsParser(builder, [
+      jsParser = new JsParser(builder, [
         htmlTemplateExtractor(htmlParser)
       ]);
     });
 
     test('HTML inside a template literal', () => {
-      outerJsParser.parseString(`
+      jsParser.parseString(`
       #
       #
       #
@@ -53,10 +39,10 @@ describe('JS: HTML template extractor', () => {
       #
       let tuce = \`
           <div>
-              {{ service.getText('First level') }}
+              <translate> First level </translate>
               <div>
-                  {{ service.getText('Second level') }}
-                  <div> {{ service.getText('Third level') }} </div>
+                  <translate> Second level </translate>
+                  <div> <translate> Third level </translate> </div>
               </div>
           </div>\`
       `, 'test')
